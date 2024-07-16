480ff8f317e4305a157862463bb07ddc0242bea9
rpm4 <rpmcdonnell@wisc.edu>
2024-07-15 20:24:39 -0500

    edits

--- figures/wmels.py
+++ figures/wmels.py
@@ -60,23 +60,35 @@ if hdfg:
     fig.label_columns(['$(\mathsf{b})$'], font_size = 10)
 
 if timeorderedwmel:
-    fig = WMEL.Artist([2,1], [0, 0.2, 0.6, 1], state_names=("$\mathsf{| g\\rangle}$",
+    fig = WMEL.Artist([4,1], [0, 0.2, 0.4, 0.8, 1], state_names=("$\mathsf{| g\\rangle}$",
                                                                    "$\mathsf{|m\\rangle}$", 
-                                                                   "$\mathsf{|v''\\rangle}$",
-                                                                   "$\mathsf{|n\\rangle}$"), number_of_interactions=5, state_font_size=9, state_text_buffer = 0.6, virtual = [1,3])
+                                                                   "$\mathsf{|v\\rangle}$",
+                                                                   "$\mathsf{|n\\rangle}$",
+                                                                   "$\mathsf{|p\\rangle}$"), number_of_interactions=5, state_font_size=9, state_text_buffer = 0.6, virtual = [1,3,4])
 
     fig.add_arrow([0,0], 0, [0,2], kind='ket', color="k", label = "$\mathsf{2}$", head_length = 0.075)
     fig.add_arrow([0,0], 1, [0,1], kind='bra', color="k", label = "$\mathsf{1}$", head_length = 0.075)
-    fig.add_arrow([0,0], 2, [2,3], kind='ket', color="k", label = "$\mathsf{3}$", head_length = 0.075)
-    fig.add_arrow([0,0], 3, [3,1], kind='out', color="blue", label = "$\mathsf{4}$", head_length = 0.075)
+    fig.add_arrow([0,0], 2, [2,4], kind='ket', color="k", label = "$\mathsf{3}$", head_length = 0.075)
+    fig.add_arrow([0,0], 3, [4,1], kind='out', color="blue", label = "$\mathsf{4}$", head_length = 0.075)
     
     
     fig.add_arrow([1,0], 0, [0,2], kind='ket', color="k", label = "$\mathsf{2}$", head_length = 0.075)
     fig.add_arrow([1,0], 1, [2,1], kind='ket', color="k", label = "$\mathsf{1}$", head_length = 0.075)
     fig.add_arrow([1,0], 2, [1,3], kind='ket', color="k", label = "$\mathsf{3}$", head_length = 0.075)
     fig.add_arrow([1,0], 3, [3,0], kind='out', color="blue", label = "$\mathsf{4}$", head_length = 0.075)
+    
+    fig.add_arrow([2,0], 0, [0,2], kind='ket', color="k", label = "$\mathsf{2}$", head_length = 0.075)
+    fig.add_arrow([2,0], 1, [2,4], kind='ket', color="k", label = "$\mathsf{3}$", head_length = 0.075)
+    fig.add_arrow([2,0], 2, [0,1], kind='bra', color="k", label = "$\mathsf{1}$", head_length = 0.075)
+    fig.add_arrow([2,0], 3, [4,1], kind='out', color="blue", label = "$\mathsf{4}$", head_length = 0.075)
+    
+    
+    fig.add_arrow([3,0], 0, [0,2], kind='ket', color="k", label = "$\mathsf{2}$", head_length = 0.075)
+    fig.add_arrow([3,0], 1, [2,4], kind='ket', color="k", label = "$\mathsf{3}$", head_length = 0.075)
+    fig.add_arrow([3,0], 2, [4,3], kind='ket', color="k", label = "$\mathsf{1}$", head_length = 0.075)
+    fig.add_arrow([3,0], 3, [3,0], kind='out', color="blue", label = "$\mathsf{4}$", head_length = 0.075)
 
-    fig.label_columns(['$(\mathsf{a})$','$(\mathsf{b})$'], font_size = 10)
+    fig.label_columns(['$(\mathsf{a})$','$(\mathsf{b})$', '$(\mathsf{c})$', '$(\mathsf{d})$'], font_size = 10)
     
     plt.savefig("timeorderedwmel.png", bbox_inches='tight')
     
